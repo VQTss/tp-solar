e2ea1ff66852c666696d00d6b0e663a91fd8f0ad
thaivo <thai.vo@wolfsolutions.vn>
2023-07-05 08:36:37 +0700

    error app key and app id

--- src/auth/check.auth.js
+++ src/auth/check.auth.js
@@ -5,14 +5,21 @@ const apiKey = async (req, res, next) => {
     const appKey = req.headers[HEADER.API_KEY]?.toString();
     const appID = req.headers[HEADER.API_ID]?.toString();
     if (!appKey) {
-        throw new BadRequestError("Error: App Key is not in header");
+        return res.status(400).json({
+            message: "Error: App Key is not in header"
+        });
     }
     if (!appID) {
-        throw new BadRequestError("Error: App ID is not in header");
+        return res.status(400).json({
+            message: "Error: App ID is not in header"
+        });
     }
 
     if (appKey !== process.env.API_KEY && appID !== process.env.API_ID) {
-        throw new BadRequestError("You do not have permission to access this app (appKey and appID)!!!");
+        return res.status(400).json({
+            message : "You do not have permission to access this app (appKey and appID)!!!"
+        });
+       
     }
 
     const objKey = {
