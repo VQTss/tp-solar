048e5a579dd80d58b4b033b0019c8874cf75ef58
thaivo <thai.vo@wolfsolutions.vn>
2023-07-06 15:18:53 +0700

    limit 50mb

--- src/app.js
+++ src/app.js
@@ -15,11 +15,9 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
-app.use(cookieParser({
-    limit : '50mb',
-}));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true }));
 
 // Morgan
 
