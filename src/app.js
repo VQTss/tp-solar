13356950ebcd342e766fc775e37164a4961bdd63
thaivo <thai.vo@wolfsolutions.vn>
2023-07-06 15:20:46 +0700

    body parser

--- src/app.js
+++ src/app.js
@@ -16,8 +16,8 @@ app.use(cors());
 
 
 app.use(cookieParser());
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true }));
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 
 // Morgan
 
