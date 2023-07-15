2a3469f36d363db67fea74e8f4b539ca895c62a2
thaivo <thaivo.software@gmail.com>
2023-07-15 14:46:11 +0700

    fix

--- src/services/order.services.js
+++ src/services/order.services.js
@@ -139,8 +139,8 @@ const OrderServices = {
             //     ],
             //     attributes: ['order_id', 'order_total', 'order_status', 'order_date', 'user_id'],
             // });
-            const [result,data] = db.sequelize.query('SELECT * FROM orders LEFT JOIN order_details ON order_details.order_id = orders.order_id LEFT JOIN products ON products.product_id = order_details.product_id')
-            return result;
+            const data = db.sequelize.query('SELECT * FROM orders LEFT JOIN order_details ON order_details.order_id = orders.order_id LEFT JOIN products ON products.product_id = order_details.product_id')
+            return data;
         } catch (error) {
             return error;
         }
