88a54776d279fc0789a77d77a246e74a802d2a89
thaivo <thaivo.software@gmail.com>
2023-07-15 23:06:57 +0700

    fix

--- models/imageProduct.model.js
+++ models/imageProduct.model.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
         }
     },
         image_base64 : {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
             allowNull: false,
             validate: {
                 notNull: { msg: "Image base64 cannot be null" },
--- models/imageSlider.model.js
+++ models/imageSlider.model.js
@@ -15,7 +15,7 @@ module.exports = (sequelize, DataTypes) => {
             }
         },
         slider_image: {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
             allowNull: false,
             validate: {
                 notNull: { msg: "Slider image cannot be null" },
