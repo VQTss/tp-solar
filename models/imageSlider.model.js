24363298bbfa4de269d72241037ca7427e64de5c
thaivo <thaivo.software@gmail.com>
2023-07-15 23:02:55 +0700

    fix

--- models/imageSlider.model.js
+++ models/imageSlider.model.js
@@ -15,7 +15,7 @@ module.exports = (sequelize, DataTypes) => {
             }
         },
         slider_image: {
-            type: DataTypes.LONGTEXT,
+            type: DataTypes.STRING,
             allowNull: false,
             validate: {
                 notNull: { msg: "Slider image cannot be null" },
